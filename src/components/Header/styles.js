8ed42e9d11c3bc5c898cb054cefa33e0d7d78f32
marco-amorim <marcoamorim95@hotmail.com>
2021-01-23 02:42:14 -0300

    new sign in button on mobile

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -29,7 +29,7 @@ export const HeaderLogin = styled.button`
 	justify-content: center;
 	align-items: center;
 	position: absolute;
-	top: 10px;
+	top: 15px;
 	right: 10px;
 	padding: 10px 15px 10px 15px;
 	border: none;
@@ -44,10 +44,5 @@ export const HeaderLogin = styled.button`
 	&:hover {
 		color: var(--white);
 		background-color: var(--bg-button-hover);
-	}
-
-	@media (max-width: 767px) {
-		position: unset;
-		margin-bottom: 30px;
 	}
 `;
